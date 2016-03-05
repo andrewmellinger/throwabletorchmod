7f6d7eafb71bb0e62a41780918b902c6b310099e
Andrew Mellinger <andrew.mellinger@gmail.com>
2016-03-05 14:01:26 -0500

    Updated to 1.8.9.

--- src/main/java/com/crashbox/throwabletorchmod/ThrowableTorchModClientProxy.java
+++ src/main/java/com/crashbox/throwabletorchmod/ThrowableTorchModClientProxy.java
@@ -1,8 +1,11 @@
 package com.crashbox.throwabletorchmod;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.Entity;
+import net.minecraftforge.fml.client.registry.IRenderFactory;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
@@ -25,8 +28,8 @@ public class ThrowableTorchModClientProxy extends ThrowableTorchModCommonProxy
 
         // Add renderers.
 
-        RenderManager renderManager = Minecraft.getMinecraft().getRenderManager();
-        RenderItem renderItem = Minecraft.getMinecraft().getRenderItem();
+        final RenderManager renderManager = Minecraft.getMinecraft().getRenderManager();
+        final RenderItem renderItem = Minecraft.getMinecraft().getRenderItem();
 
         RenderingRegistry.registerEntityRenderingHandler(EntityThrowableSlimeTorch.class,
                 new RenderSnowball(renderManager, ThrowableTorchMod.ITEM_THROWABLE_SLIME_TORCH, renderItem));
@@ -36,6 +39,37 @@ public class ThrowableTorchModClientProxy extends ThrowableTorchModCommonProxy
 
         RenderingRegistry.registerEntityRenderingHandler(EntityThrowableMagmaTorch.class,
                 new RenderSnowball(renderManager, ThrowableTorchMod.ITEM_THROWABLE_MAGMA_TORCH, renderItem));
+
+//        RenderingRegistry.registerEntityRenderingHandler(EntityThrowableSlimeTorch.class,
+//            new IRenderFactory<Entity>() {
+//                @Override
+//                public Render<? super Entity> createRenderFor(RenderManager manager)
+//                {
+//                    return new RenderSnowball<Entity>(renderManager, ThrowableTorchMod.ITEM_THROWABLE_SLIME_TORCH, renderItem);
+//                }
+//            }
+//        );
+//
+//        RenderingRegistry.registerEntityRenderingHandler(EntityThrowableClayTorch.class,
+//            new IRenderFactory<Entity>()
+//            {
+//                @Override
+//                public Render<? super Entity> createRenderFor(RenderManager manager)
+//                {
+//                   return  new RenderSnowball<Entity>(renderManager, ThrowableTorchMod.ITEM_THROWABLE_CLAY_TORCH, renderItem);
+//                }
+//            }
+//        );
+//
+//        RenderingRegistry.registerEntityRenderingHandler(EntityThrowableMagmaTorch.class,
+//            new IRenderFactory<Entity>() {
+//                @Override
+//                public Render<? super Entity> createRenderFor(RenderManager manager)
+//                {
+//                    return new RenderSnowball<Entity>(renderManager, ThrowableTorchMod.ITEM_THROWABLE_MAGMA_TORCH, renderItem);
+//                }
+//            }
+//        );
     }
 
     @Override
